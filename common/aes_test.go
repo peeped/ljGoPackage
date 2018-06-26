edd59166d80c39e262208e5accb380dad75d5aa4
Liao Jie <liaojie@outlook.com>
2018-06-26 11:02:40 +0800

    modified:   common/aes_test.go

--- common/aes_test.go
+++ common/aes_test.go
@@ -2,6 +2,8 @@ package common
 
 import (
 	"encoding/base64"
+	"fmt"
+	"github.com/peeped/ljGoPackage/common"
 	"testing"
 )
 
@@ -10,10 +12,16 @@ func TestAesEncrypt(t *testing.T) {
 	aesKey := []byte("d7b59483d9954929b3262acc677cb51a")
 
 	// AES加密
-	result, err := AesEncrypt([]byte("abcd@163.com"), aesKey)
+	result, err := common.AesEncrypt([]byte("abcd@163.com"), aesKey)
 	if err != nil {
 		panic(err)
 	}
-
 	t.Logf("加密后并BASE64:%v\n", base64.StdEncoding.EncodeToString(result))
+
+	// AES解密
+	origData, err := common.AesDecrypt(result, aesKey)
+	if err != nil {
+		panic(err)
+	}
+	t.Logf("解密后：:%v\n", string(origData))
 }
